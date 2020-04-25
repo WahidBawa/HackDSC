cb4821653f79f7613627f3a17c3ba429431edfce
HenryPham1546 <56170415+HenryPham1546@users.noreply.github.com>
2020-04-25 18:37:04 -0400

    Add files via upload

---
+++ HackDSC/DetailsPanel.java
@@ -0,0 +1,14 @@
+package io.agora.mainClass;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DetailsPanel extends JPanel {
+    public DetailsPanel() {
+        Dimension size = getPreferredSize();
+        size.width = 250;
+        setPreferredSize(size);
+
+        setBorder(BorderFactory.createTitledBorder("Messages"));
+    }
+}
---
+++ HackDSC/MainFrame.java
@@ -0,0 +1,40 @@
+package io.agora.mainClass;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame {
+
+    private DetailsPanel detailsPanel;
+
+    public MainFrame(String title){
+        super(title);
+
+        //Set layout manager
+        setLayout(new BorderLayout());
+
+        //Create Swing Component
+        final JTextArea textArea = new JTextArea();
+        JButton button  = new JButton("Click me!");
+
+        detailsPanel = new DetailsPanel();
+        //Add Swing components to content pane
+        Container c = getContentPane();
+
+        c.add(textArea, BorderLayout.CENTER);
+        c.add(button, BorderLayout.SOUTH);
+        c.add(detailsPanel , BorderLayout.WEST);
+
+        //Add behaviour
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textArea.append("hello\n");
+            }
+        });
+
+    }
+}
